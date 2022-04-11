4ddb06a846c76c2f8188e6986919668310af0fd5
MiguelC240 <mikicortes1@gmail.com>
2022-04-10 19:27:32 -0500

    .

--- Practica2_19110127.py
+++ Practica2_19110127.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 import math
 
+import numpy as np
+import skimage
+from skimage import io
 
 
 Img1 = cv2.imread('Imagen_Dia.jpg')
@@ -11,10 +13,10 @@ Img2 = cv2.imread('Imagen_Noche.jpg')
 Img3 = cv2.imread('Imagen_Noche.jpg',0)
 
 
-res1 = cv2.resize(Img1, dsize=(380, 380))
+res1 = cv2.resize(Img1, dsize=(280, 280))
 cv2.imshow('Img1',res1)
 
-res2 = cv2.resize(Img2, dsize=(380, 380))
+res2 = cv2.resize(Img2, dsize=(280, 280))
 cv2.imshow('Img2',res2)
 
 
@@ -162,7 +164,157 @@ if M == ord('m'):
     cv2.imshow('Pote',Pote)
     cv2.waitKey()
     cv2.destroyWindow('Pote')
+
+
+###################### CONJUNCIÓN ##############################
+
+    #Conjuncion
+    conjuncion = cv2.bitwise_and(res1,res2)
+    cv2.imshow('Conjuncion', conjuncion)
+    cv2.waitKey()
+    cv2.destroyWindow('Conjuncion')
+
+
+###################### DISYUNCIÓN ##############################
+
+    #Disyuncion
+    disyuncion = cv2.bitwise_or(res1,res2)
+    cv2.imshow('Disyuncion', disyuncion)
+    cv2.waitKey()
+    cv2.destroyWindow('Disyuncion')
+
+
+###################### NEGACIÓN ##############################
+
+    #Negación 1
+    Negacion = cv2.resize(Img1, dsize=(280, 280))
+    height, width, _ = Negacion.shape
+
+    for i in range(0, height - 1):
+        for j in range(0, width -1):
+            pixel = Negacion[i,j]
+            pixel[0] = 255 - pixel[0]
+            pixel[1] = 255 - pixel[1]
+            pixel[2] = 255 - pixel[2]
+            Negacion[i,j] = pixel
+    cv2.imshow('Negacion1',Negacion)
+    cv2.waitKey()
+    cv2.destroyWindow('Negacion1')
+
+
+    #Negación 2
+    Nega = 1 - res1
+    cv2.imshow('Negacion2',Nega)
+    cv2.waitKey()
+    cv2.destroyWindow('Negacion2')
+
+
+###################### TRASLACIÓN ##############################
     
+    #Traslación
+    ancho = res1.shape[1] #columnas
+    alto = res1.shape[0] # filas
+    
+    M = np.float32([[1,0,10],[0,1,100]]) #Construccion de la matriz
+    traslacion = cv2.warpAffine(res1,M,(ancho,alto))
+    cv2.imshow('Traslacion',traslacion)
+    cv2.waitKey()
+    cv2.destroyWindow('Traslacion')
+
+
+###################### RESCALADO ##############################
+
+    #Rescalado
+    Rescalado = cv2.resize(Img1, dsize=(480, 480))
+    cv2.imshow('Rescalado',Rescalado)
+    cv2.waitKey()
+    cv2.destroyWindow('Rescalado')
+
+
+
+###################### ROTACION ##############################
+
+    # Rotación
+    Noche = io.imread("Imagen_Noche.jpg")
+    type(Noche)
+    Noche.shape
+    plt.imshow(Noche[::-1])###Invertir imagen
+    plt.show()
+
+
+    # Rotación
+    ancho = res2.shape[1] #columnas
+    alto = res2.shape[0] # filas
+    
+    Rotacion = cv2.getRotationMatrix2D((ancho//2,alto//2),180,1)
+    imageOut = cv2.warpAffine(res2,Rotacion,(ancho,alto))
+    cv2.imshow('Rotacion',imageOut)
+    cv2.waitKey()
+    cv2.destroyWindow('Rotacion')
+
+
+
+###################### TRASLACION A FIN ##############################
+
+    #Traslacion 1
+    traslacion1 = cv2.warpAffine(res1,M,(ancho,alto))
+    cv2.imshow('Traslacion_1',traslacion1)
+    cv2.waitKey()
+    cv2.destroyWindow('Traslacion_1')
+
+    #Traslacion 2
+    rows, cols, ch = res1.shape
+    pts1 = np.float32([[50, 50],
+                       [200, 50], 
+                       [50, 200]])
+      
+    pts2 = np.float32([[10, 100],
+                       [200, 50], 
+                       [100, 250]])
+
+    M2 = cv2.getAffineTransform(pts1, pts2)
+    dst = cv2.warpAffine(res1, M2, (cols, rows))
+    cv2.imshow('Traslacion_2',dst)
+    cv2.waitKey()
+    cv2.destroyWindow('Traslacion_2')
+
+
+    #Traslacion 3
+    rows,cols,ch = res1.shape
+     
+    pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+    pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+
+    M3 = cv2.getPerspectiveTransform(pts1,pts2)
+    tras = cv2.warpPerspective(res1,M3,(300,300))
+    cv2.imshow('Traslacion_3',tras)
+    cv2.waitKey()
+    cv2.destroyWindow('Traslacion_3')
+
+###################### TRANSPUESTA ##############################
+
+    #Transpuesta 1
+    def transponer(res1):
+        t = []
+        for i in range(len(res1[0])):
+            t.append([])
+            for j in range(len(res1)):
+                t[i].append(res1[j][i])
+        return t
+    transpuesta1 = np.concatenate((res1, transponer(res1), res2), axis=1)
+    transpuesta1 = cv2.cvtColor(transpuesta1, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Transpuesta_1', transpuesta1)
+    cv2.waitKey()
+    cv2.destroyWindow('Transpuesta_1')
+
+    #Transpuesta 2
+    trans2 = cv2.transpose(res1)
+    transpuesta2 = np.concatenate((res1, trans2, res2), axis=1)
+    transpuesta2 = cv2.cvtColor(transpuesta2, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Transpuesta_2', transpuesta2)
+    cv2.waitKey()
+    cv2.destroyWindow('Transpuesta_2')
+
     
 cv2.waitKey()
 cv2.destroyAllWindows()
